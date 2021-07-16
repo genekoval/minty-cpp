83f22aaa8ad94539b7e2de2e91bd503d9ee51afb
Gene Koval <genyaremchuk@protonmail.com>
2021-07-16 17:48:43 -0400

    Move router code into server translation unit

--- src/libserver/server.cpp
+++ src/libserver/server.cpp
@@ -1,9 +1,57 @@
-#include "router/router.h"
+#include "context/context.h"
 
 #include <minty/net/zipline/transfer.h>
+#include <minty/net/zipline/protocol.h>
 #include <minty/server/server.h>
 
-#include <timber/timber>
+using minty::server::context;
+
+template <typename ...Routes>
+using router = zipline::router<
+    minty::net::socket,
+    minty::net::event_t,
+    minty::net::error_list,
+    context,
+    Routes...
+>;
+
+namespace {
+    auto make_router(context& ctx) {
+        return router(
+            ctx,
+            &context::add_comment,
+            &context::add_object_data,
+            &context::add_object_local,
+            &context::add_objects_url,
+            &context::add_post,
+            &context::add_post_objects,
+            &context::add_post_tag,
+            &context::add_tag,
+            &context::add_tag_alias,
+            &context::add_tag_source,
+            &context::delete_post,
+            &context::delete_post_objects,
+            &context::delete_post_tag,
+            &context::delete_tag,
+            &context::delete_tag_alias,
+            &context::delete_tag_source,
+            &context::get_comments,
+            &context::get_object,
+            &context::get_post,
+            &context::get_server_info,
+            &context::get_tag,
+            &context::get_tag_posts,
+            &context::get_tag_previews,
+            &context::get_tags_by_name,
+            &context::move_post_object,
+            &context::set_comment_content,
+            &context::set_post_description,
+            &context::set_post_title,
+            &context::set_tag_description,
+            &context::set_tag_name
+        );
+    }
+}
 
 namespace minty::server {
     auto listen(
