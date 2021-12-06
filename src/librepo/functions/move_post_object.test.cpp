b5cf2073c2acb67a62c35b8aa566c6c3b70a453b
Gene Koval <genyaremchuk@protonmail.com>
2021-12-06 00:53:45 -0500

    Test sequence numbers of moved objects

--- src/librepo/functions/move_post_object.test.cpp
+++ src/librepo/functions/move_post_object.test.cpp
@@ -1,29 +1,19 @@
 #include "database.test.h"
 
 TEST_F(DatabasePostObjectTest, MoveObject) {
-    const auto objects = std::vector<std::string> {
-        "0e5bad49-9cf9-46e4-85c9-83ef9baf1f79",
-        "adf85243-7673-467f-9fa6-56fc40642e06",
-        "0797323f-910f-4f90-9bb1-dd145730c7a0",
-        "fc6478d2-2acb-4773-82c9-fbd4f756303d",
-        "edfd4b08-334a-4969-8fe7-0acc15a77045"
-    };
+    database.move_post_object(post_id, 2, 0);
 
-    for (const auto& object : objects) {
-        database.create_object(object, {}, {});
-    }
+    const auto result = with_sequence();
 
-    const auto post = database.create_post("", "", objects, {}).id;
+    ASSERT_EQ(3, result.size());
 
-    database.move_post_object(post, 3, 0);
-
-    const auto result = database.read_post_objects(post);
-
-    ASSERT_EQ(objects[3], result[0].id);
+    ASSERT_EQ(objects[2], result[0].id);
     ASSERT_EQ(objects[0], result[1].id);
     ASSERT_EQ(objects[1], result[2].id);
-    ASSERT_EQ(objects[2], result[3].id);
-    ASSERT_EQ(objects[4], result[4].id);
+
+    ASSERT_EQ(1, result[0].sequence);
+    ASSERT_EQ(2, result[1].sequence);
+    ASSERT_EQ(3, result[2].sequence);
 }
 
 TEST_F(DatabasePostObjectTest, MovePostObjectDateModified) {
